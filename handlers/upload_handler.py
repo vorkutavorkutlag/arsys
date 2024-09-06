81c509a5a791db9192cab47aecf867dfeba307e7
vorkutavorkutlag <royzurakipiani@gmail.com>
2024-09-06 19:17:15 +0300

    Changed to absolute paths to be able to run this from cmd. Closed aiohttp session.

--- handlers/upload_handler.py
+++ handlers/upload_handler.py
@@ -1,4 +1,6 @@
 import os
+
+import aiohttp
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
@@ -13,13 +15,21 @@ load_dotenv()
 
 class Uploader:
 
+    def __init__(self, creds: list, ROOT_DIR):
+        self.ROOT_DIR = ROOT_DIR
+        self.token,\
+        self.refresh_token,\
+        self.client_id,\
+        self.client_secret,\
+        self.token_uri = creds
+
     @staticmethod
-    def upload_video(youtube, file_path, title, description, tags, category="22", privacy_status="private"):
+    def upload_video(youtube, file_path, title, tags, category="24", privacy_status="public"):
         try:
             body = {
                 "snippet": {
                     "title": title,
-                    "description": description,
+                    "description": "",
                     "tags": tags,
                     "categoryId": category
                 },
@@ -43,14 +53,13 @@ class Uploader:
             return None
 
 
-    @staticmethod
-    def authenticate_youtube():
+    def authenticate_youtube(self):
         creds = Credentials(
-            token=os.getenv('YOUTUBE_ACCESS_TOKEN'),
-            refresh_token=os.getenv('YOUTUBE_REFRESH_TOKEN'),
-            client_id=os.getenv('YOUTUBE_CLIENT_ID'),
-            client_secret=os.getenv('YOUTUBE_CLIENT_SECRET'),
-            token_uri=os.getenv('YOUTUBE_TOKEN_URI')
+            token=self.token,
+            refresh_token=self.refresh_token,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            token_uri=self.token_uri
         )
 
         # Refresh the token if it's expired
@@ -59,37 +68,46 @@ class Uploader:
 
         return build('youtube', 'v3', credentials=creds)
 
-    @staticmethod             # WILL PROMPT YOU TO LOG INTO YOUR ACCOUNT TO GET TOKENS. SHOULD BE USED ONLY ONCE
-    def get_tokens():
-        SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
-        flow = InstalledAppFlow.from_client_config({
-            "installed": {
-                "client_id": os.getenv('YOUTUBE_CLIENT_ID'),
-                "client_secret": os.getenv('YOUTUBE_CLIENT_SECRET'),
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": os.getenv('YOUTUBE_TOKEN_URI'),
-                "redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]
-            }
-        }, SCOPES)
-
-        # Launch the OAuth flow and get credentials
-        creds = flow.run_local_server(port=0)
-
-        print(f"Access Token: {creds.token}")
-        print(f"Refresh Token: {creds.refresh_token}")
-
-    def upload_videos_from_folder(self, folder_path, title, description, tags):
-        youtube = self.authenticate_youtube()
-        num_uploaded = 0
-        for file_name in os.listdir(folder_path):
-            if file_name.endswith(('.mp4', '.avi', '.mov', '.mkv')):
-                file_path = os.path.join(folder_path, file_name)
-                self.upload_video(youtube, file_path, title, description, tags)
-                num_uploaded += 1
+    async def upload_videos_from_folder(self, folder_path, tags):
+        upload_url = 'https://www.googleapis.com/upload/youtube/v3/videos?uploadType=resumable'
+        async with aiohttp.ClientSession() as session:
+            youtube = self.authenticate_youtube()
+            num_uploaded = 0
+            for file_name in os.listdir(os.path.join(self.ROOT_DIR, folder_path)):
+                if file_name.endswith('.mp4'):
+                    file_path = os.path.join(self.ROOT_DIR, folder_path, file_name)
+                    title = file_name.replace("_", " ").strip(".mp4")
+                    self.upload_video(youtube, file_path, title, tags)
+                    num_uploaded += 1
+            await session.post(upload_url, data={})
         return num_uploaded
 
 
+
+def get_tokens(client_id, client_secret, token_uri):          # WILL PROMPT YOU TO LOG INTO YOUR ACCOUNT TO GET TOKENS. SHOULD BE USED ONLY ONCE
+    SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+
+    flow = InstalledAppFlow.from_client_config({
+        "installed": {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": token_uri,
+            "redirect_uris": ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"]
+        }
+    }, SCOPES)
+
+    # Launch the OAuth flow and get credentials
+    creds = flow.run_local_server(port=0)
+
+    print(f"Access Token: {creds.token}")
+    print(f"Refresh Token: {creds.refresh_token}")
+
+
 if __name__ == "__main__":
-    uploader = Uploader()
-    uploader.get_tokens()
+    clientid = input("client id\n")
+    clientsecret = input("client secret\n")
+    tokenuri = input("token uri\n")
+
+    get_tokens(clientid, clientsecret, tokenuri)
